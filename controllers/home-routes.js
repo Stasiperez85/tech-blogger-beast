6170bfdffa3b5d674bdc5be23905077c4d8966ea
Anastasia Perez <Stasiperez85@gmail.com>
2022-03-17 21:32:23 -0600

    update cookies and auth

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,45 +2,49 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User } = require('../models');
 
-router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        title: 'Handlebars Docs',
-        content: "test",
-        created_at: new Date(),
-        user: {
-            username: 'test_user'
-        }
-    });
-});
-
 // router.get('/', (req, res) => {
-//     Post.findAll({
-//         attributes: [
-//             'id',
-//             'content',
-//             'title',
-//             'created_at',
-//             // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//         ],
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//         .then(dbPostData => {
-//             // pass a single post object into the homepage template
-//             console.log(dbPostData[0]);
-//             const posts = dbPostData.map(post => post.get({ plain: true }));
-//             res.render('homepage', { posts });
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
+//     console.log("hello!")
+//     res.render('homepage', {
+//         id: 1,
+//         title: 'Handlebars Docs',
+//         content: "test",
+//         created_at: new Date(),
+//         user: {
+//             username: 'test_user'
+//         }
+//     });
 // });
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'content',
+            'title',
+            'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            console.log(dbPostData[0]);
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
